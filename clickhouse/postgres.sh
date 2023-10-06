c404503df9a1c1e97436321109ada36cc79fcf59
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-06 18:22:56 +0200

    Update postgres.sh

--- clickhouse/postgres.sh
+++ clickhouse/postgres.sh
@@ -5,4 +5,4 @@ sleep 10
 
 # Run ClickHouse client commands
 clickhouse-client
-clickhouse client --query="CREATE DATABASE homeassistant ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant') SETTINGS materialized_postgresql_tables_list = 'table1';"
+clickhouse client --query="CREATE DATABASE home ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant', '', '') SETTINGS materialized_postgresql_tables_list = 'table1';"
