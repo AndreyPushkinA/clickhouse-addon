ddff2cc46eac07b1be21ed035f9fae8822b73ca1
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-06 18:02:21 +0200

    Create postgres.sh

---
+++ clickhouse/postgres.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Wait for ClickHouse server to start. Adjust the sleep time as needed.
+sleep 10
+
+# Run ClickHouse client commands
+clickhouse-client
+clickhouse client --query="CREATE DATABASE homeassistant ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant') SETTINGS materialized_postgresql_tables_list = 'table1';"
