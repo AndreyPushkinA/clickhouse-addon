6fe1d709a1a244d04eef48a5cdfe09eef9ca4c39
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-08 15:58:46 +0200

    Delete clickhouse/postgres.sh

--- clickhouse/postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Wait for ClickHouse server to start. Adjust the sleep time as needed.
-sleep 10
-
-# Run ClickHouse client commands
-clickhouse-client
-clickhouse client --query="CREATE DATABASE home ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant', '', '') SETTINGS materialized_postgresql_tables_list = 'table1';"
